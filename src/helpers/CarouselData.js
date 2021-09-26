46e1bf85107519323950ccfb2b430c4536c03c14
Gabriel Rezes <gabriel.rezes@gmail.com>
2021-09-25 22:25:33 -0300

    carrosel de imagens

---
+++ src/helpers/CarouselData.js
@@ -0,0 +1,45 @@
+//ambience
+import img0101 from '../assets/img/carousel/ambience/img0101.jpg';
+import img0201 from '../assets/img/carousel/ambience/img0201.jpg';
+import img0301 from '../assets/img/carousel/ambience/img0301.jpg';
+import img0401 from '../assets/img/carousel/ambience/img0401.jpg';
+import img0501 from '../assets/img/carousel/ambience/img0501.jpg';
+
+//sharedArea
+import img0102 from '../assets/img/carousel/sharedArea/img0102.jpg';
+import img0202 from '../assets/img/carousel/sharedArea/img0202.jpg';
+import img0302 from '../assets/img/carousel/sharedArea/img0302.jpg';
+import img0402 from '../assets/img/carousel/sharedArea/img0402.jpg';
+import img0502 from '../assets/img/carousel/sharedArea/img0502.jpg';
+
+//sharedArea
+import img0103 from '../assets/img/carousel/bedRooms/img0103.jpg';
+import img0203 from '../assets/img/carousel/bedRooms/img0203.jpg';
+import img0303 from '../assets/img/carousel/bedRooms/img0303.jpg';
+import img0403 from '../assets/img/carousel/bedRooms/img0403.jpg';
+import img0503 from '../assets/img/carousel/bedRooms/img0503.jpg';
+
+export const ambience = [
+    { img: img0101 },
+    { img: img0201 },
+    { img: img0301 },
+    { img: img0401 },
+    { img: img0501 },
+];
+
+export const sharedArea = [
+    { img: img0102 },
+    { img: img0202 },
+    { img: img0302 },
+    { img: img0402 },
+    { img: img0502 },
+];
+
+export const bedRooms = [
+    { img: img0103 },
+    { img: img0203 },
+    { img: img0303 },
+    { img: img0403 },
+    { img: img0503 },
+];
+
